6a7dbcbe89bb9d8ab0e30f927ca7f9a13a1a3281
itscrystalline <pvpthadgaming@gmail.com>
2026-04-27 13:38:09 +0300

    feat(backend): sketch oyt some trait fnd

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,9 +1,31 @@
+use crate::ScreenCoordinates;
+
 #[cfg(feature = "async")]
-pub mod async_backend {
-    #[async_trait::async_trait]
-    pub trait AsyncLcdBackend {}
+#[allow(async_fn_in_trait)]
+pub trait AsyncLcdBackend<const CHAR_HEIGHT: usize> {
+    const CUSTOM_CHARACTER_SLOTS: u8;
+    type Error;
+
+    async fn init_driver(&mut self) -> Result<&mut Self, Self::Error>;
+    async fn clear(&mut self) -> Result<&mut Self, Self::Error>;
+    async fn move_cursor(&mut self, pos: ScreenCoordinates) -> Result<&mut Self, Self::Error>;
+    async fn write_byte(&mut self, byte: u8) -> Result<&mut Self, Self::Error>;
+    async fn set_custom_character(
+        &mut self,
+        character: [u8; CHAR_HEIGHT],
+    ) -> Result<u8, [u8; CHAR_HEIGHT]>;
 }
 
-pub mod sync_backend {
-    pub trait LcdBackend {}
+pub trait LcdBackend<const CHAR_HEIGHT: usize> {
+    const CUSTOM_CHARACTER_SLOTS: u8;
+    type Error;
+
+    fn init_driver(&mut self) -> Result<&mut Self, Self::Error>;
+    fn clear(&mut self) -> Result<&mut Self, Self::Error>;
+    fn move_cursor(&mut self, pos: ScreenCoordinates) -> Result<&mut Self, Self::Error>;
+    fn write_byte(&mut self, byte: u8) -> Result<&mut Self, Self::Error>;
+    fn set_custom_character(
+        &mut self,
+        character: [u8; CHAR_HEIGHT],
+    ) -> Result<u8, [u8; CHAR_HEIGHT]>;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # liquid_crystal_ui
+//! A retained-mode-esque UI library for displaying to small alphanumeric LCD displays, like the
+//! ones on cash registers or something
+
 #![no_std]
 
 #[cfg(feature = "alloc")]
@@ -5,3 +9,19 @@ extern crate alloc;
 
 pub mod backend;
 pub mod ui;
+
+pub struct ScreenCoordinates {
+    row: u8,
+    col: u8,
+}
+
+impl ScreenCoordinates {
+    pub fn at(x: u8, y: u8) -> Self {
+        ScreenCoordinates { row: x, col: y }
+    }
+}
+impl From<(u8, u8)> for ScreenCoordinates {
+    fn from(value: (u8, u8)) -> Self {
+        ScreenCoordinates::at(value.0, value.1)
+    }
+}
